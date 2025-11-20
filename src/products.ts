bf5405d76259e8fe6ccaaf5bb6b340a5bd29de87
Lovexyco <162183324+Lovexyco@users.noreply.github.com>
2025-11-21 01:33:20 +0630

    Add products data file

---
+++ src/products.ts
@@ -0,0 +1,11 @@
+export const products = [
+  {
+    id: "olo-ultra-thin-01",
+    name: "OLO 0.01mm Ultra Thin Condom",
+    price: 25000,
+    box: "10 pcs Box",
+    size: "52mm Dia, 160mm Length",
+    description: "High quality ultra thin condom with warming lubrication. 0.01mm thin, hyaluronic coated, smooth feeling.",
+    image: "https://i.ibb.co/wZcx0Zgj/your-image.jpg"
+  }
+];
